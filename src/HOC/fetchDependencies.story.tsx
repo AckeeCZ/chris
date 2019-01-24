94a2f8564aff3709bf9072342f35c4215a61d75d
Lukas Horak <horaklukas@centrum.cz>
2019-01-24 17:15:30 +0100

    Add fetchDepenedncies story and export it from pacage

---
+++ src/HOC/fetchDependencies.story.tsx
@@ -0,0 +1,129 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { createBrowserHistory } from 'history';
+import { storiesOf } from '@storybook/react';
+import { AnyAction, compose, bindActionCreators, Dispatch, ActionCreator } from 'redux';
+import { connect, Provider as StoreProvider } from 'react-redux';
+
+import { configureStore } from '../../stories';
+import fetchDependencies from './fetchDependencies';
+
+export const history = createBrowserHistory();
+
+const initialState = {
+    users: {
+        data: [],
+    },
+};
+
+const store = configureStore(
+    initialState,
+    {
+        users: (state = { data: [] }, action: AnyAction) => {
+            switch (action.type) {
+                case 'SET_DATA':
+                    return {
+                        data: action.payload,
+                    };
+                case 'CLEAR_DATA':
+                    return {
+                        data: [],
+                    };
+                default:
+                    return state;
+            }
+        },
+    },
+);
+
+const fetchAndSetData = (readCount: number = 2) => ({
+    type: 'SET_DATA',
+    payload: [{ id: 1, name: 'Franta Vomáčka' }, { id: 2, name: 'Julian Zápotocký' }].slice(0, readCount),
+});
+
+const clearData = () => ({
+    type: 'CLEAR_DATA',
+});
+
+interface User {
+    id: number;
+    name: string;
+}
+
+interface Props {
+    users: User[];
+    loadingText: string;
+}
+
+type ComponentProps = Pick<Props, 'loadingText'>;
+
+const UsersList = ({ users, loadingText }: Props) => (
+    <ul>
+        {users.length === 0 && loadingText}
+        {users.map((user: User) => (
+            <li key={user.id}>{user.name}</li>
+        ))}
+    </ul>
+);
+
+UsersList.propTypes = {
+    users: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.number.isRequired,
+            name: PropTypes.string.isRequired,
+        }),
+    ).isRequired,
+    loadingText: PropTypes.string.isRequired,
+};
+
+interface State {
+    users: {
+        data: User[];
+    };
+}
+
+const delayedDispatch = (dispatch: Dispatch, reduxAction: ActionCreator<any>) => {
+    return (...args: any[]) => {
+        setTimeout(() => dispatch(reduxAction(...args)), 3000);
+    };
+};
+
+storiesOf('HOC/fetchDependencies', module)
+    .addDecorator(story => (
+        <StoreProvider store={store}>
+            {story()}
+        </StoreProvider>
+    ))
+    .add('default load handler', () => {
+        const UsersListContainer = compose<React.SFC<ComponentProps>>(
+            connect(
+                (state: State) => ({ users: state.users.data }),
+                dispatch => ({
+                    fetch: delayedDispatch(dispatch, fetchAndSetData),
+                    clear: bindActionCreators(clearData, dispatch),
+                }),
+            ),
+            fetchDependencies(),
+        )(UsersList);
+
+        return <UsersListContainer loadingText="Načítám všechny uživatele..." />;
+    })
+    .add('custom load handler', () => {
+        const UsersListContainer = compose<React.SFC<ComponentProps>>(
+            connect(
+                (state: State) => ({ users: state.users.data }),
+                dispatch => ({
+                    fetch: delayedDispatch(dispatch, fetchAndSetData),
+                }),
+            ),
+            fetchDependencies({
+                onLoad: ({ fetch }) => {
+                    if (typeof fetch === 'function') {
+                        fetch(1);
+                    }
+                },
+            }),
+        )(UsersList);
+
+        return <UsersListContainer loadingText="Načítám prvního uživatele..." />;
+    });
--- src/HOC/index.ts
+++ src/HOC/index.ts
@@ -1,1 +1,2 @@
+export { default as fetchDependencies } from './fetchDependencies';
 export { default as routeDependencies } from './routeDependencies';
