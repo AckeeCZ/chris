76c98dd72b5c5f2f4318263c5cc4a21d06cf84b5
Jiří Čermák <jiri.cermak@ackee.cz>
2019-04-25 23:54:05 +0200

    Optimaze lodash partial imports

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,18 @@
 module.exports = {
     presets: ['@babel/typescript', '@babel/env', '@babel/react'],
-    plugins: ['@babel/proposal-class-properties', '@babel/proposal-object-rest-spread'],
+    plugins: [
+        '@babel/proposal-class-properties',
+        '@babel/proposal-object-rest-spread',
+        [
+            'babel-plugin-transform-imports',
+            {
+                lodash: {
+                    // eslint-disable-next-line
+                    transform: 'lodash/${member}',
+                    preventFullImport: true,
+                },
+            },
+        ],
+    ],
     ignore: ['**/__tests__/'],
 };
